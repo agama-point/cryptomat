39862a3444756c2ca984c119f37c06260ddd26ed
Honza S. Copak <bonum.comunae@volny.cz>
2017-09-21 17:43:48 +0200

    Add files via upload

--- octopusEngineLib/oeUtil.py
+++ octopusEngineLib/oeUtil.py
@@ -7,9 +7,13 @@ import json
 
 
 #---server time--- (start > RPi was off-line)
-def getServerTime():
-   serverTime = urllib.request.urlopen("http://www.octopusengine.eu/api/datetime.php").read()
-   return str(serverTime.decode('ascii'))
+def getServerTime():     
+     try:
+         serverTime = urllib.request.urlopen("http://www.octopusengine.eu/api/datetime.php").read()        
+     except:
+         serverTime = "Err"
+         print("Err. wifi srever time")
+     return str(serverTime.decode('ascii'))   
 
 def addLog(txtLog):
   print("LOG:"+txtLog)
